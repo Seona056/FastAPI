5066dc539403c6c8d96618ea5dce4849ee19c435
Seona056 <pinotnoir056@outlook.com>
2023-04-04 20:21:13 +0900

    1-07

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()     # FastAPI 클래스 인스턴스
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_headers=["*"],
+)
+
+@app.get("/hello")      # 어노테이션: /hello라는 URL요청이 발생하면 해당 함수 리턴
+def hello():
+    return {"message":"안녕하세요 파이보"}
