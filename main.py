2afd02d1785e79fb915e6eb74912e0c8b4fad4b3
Seona056 <pinotnoir056@outlook.com>
2023-04-06 07:20:13 +0900

    2-04-1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from domain.question import question_router
+
 app = FastAPI()     # FastAPI 클래스 인스턴스
 
 origins = [
@@ -14,6 +16,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/hello")      # 어노테이션: /hello라는 URL요청이 발생하면 해당 함수 리턴
-def hello():
-    return {"message":"안녕하세요 파이보"}
+# @app.get("/hello")      # 어노테이션: /hello라는 URL요청이 발생하면 해당 함수 리턴
+# def hello():
+#     return {"message":"안녕하세요 파이보"}
+
+app.include_router(question_router.router)
