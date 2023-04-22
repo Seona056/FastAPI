dd7fb43c39741abbf2709f47824fc189860b911f
Seona056 <pinotnoir056@outlook.com>
2023-04-23 03:34:14 +0900

    2-05-3

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,7 +1,6 @@
 <script>
     import fastapi from "../lib/api"
     let question_list = []
-
     function get_question_list() {
         /*
         fetch("http://localhost:8000/api/question/list").then((response) => {
@@ -9,11 +8,12 @@
                 question_list = json
             })
         })*/
-        fastapi('get', '/api/question/list', {}, (json) => {
+        // ../lib/api.js 파일에 선언된 변수 fastapi = (operation, url, params, success_callback, failure_callback)
+        // failure_callback은 입력하지 않음 (입력되지 않았을때 alert로 표시: fastapi의 if/else문 참고)
+        fastapi('get', '/api/question/list', {}, (json) => {  // success_callback인 (jason)는 화살표 함수로 전달
             question_list = json
         })
     }
-
     get_question_list()
 </script>
 
